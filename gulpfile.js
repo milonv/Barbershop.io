7325dce09a11cd45e92dbb69299b89fdaf041692
Anastasiia <milonova.nastasia@gmail.com>
2020-10-14 10:22:17 +0300

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require("browser-sync").create();
 
 gulp.task("sass-to-css", function () {
     return gulp
         .src("src/styles/**/*.scss")
         .pipe(sass())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest("dist"));
 });
+
+// gulp.task('gulp-autoprefixer', function () {
+//     return gulp.src('dist/main.css')
+//         .pipe(autoprefixer({
+//             browsers: ['last 2 versions'],
+//             cascade: false
+//         }))
+//         .pipe(gulp.dest('dist'));
+// });
 
 gulp.task("sass-watch", function () {
     browserSync.init({
